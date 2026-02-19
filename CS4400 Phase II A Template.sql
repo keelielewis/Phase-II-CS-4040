a9048c6dae5ee01bc7b6e451170c91ee70001820
keelielewis <4keelielewis@gmail.com>
2026-02-18 19:06:31 -0500

    Add table definitions for user, subscription, and content
    some of them

--- CS4400 Phase II A Template.sql
+++ CS4400 Phase II A Template.sql
@@ -29,3 +29,71 @@ use media_streaming_service;
 data types, and check constraints) here.  You may sequence them in any order that 
 
 works for you (and runs successfully). */
+-- User
+CREATE TABLE user (
+  AccountID INT NOT NULL,
+  name Varchar(100) not null, 
+  bdate date not null,
+  email varchar(255) not null,
+  primary key (AccountId),
+  Unique (email));
+
+-- Subscription
+Create table subscription (
+  SubscriptionID INT not null,
+  start_date Date not null,
+  end_date date not null,
+  cost decimal(10,2) not null,
+  primary key (SubscriptionID));
+
+-- Individual
+CREATE TABLE individual (
+  SubscriptionID int not null,
+  tier varchar(50) not null,
+  primary key (SubscriptionID),
+  foreign key (SubscriptionID)
+      references subscription(SubscriptionID)
+      on update cascade
+      on delete cascade);
+
+-- Family
+create table family (
+  SubscriptionID int not null,
+  max_family_size int not null,
+  primary key (SubscriptionID),
+  Foreign key (SubscriptionID)
+      references subscription(SubscriptionID)
+      on update cascade
+      on delete cascade);
+
+-- Content
+create table content (
+  ContentID int not null,
+  title varchar(200) not null,
+  release_date date not null,
+  language varchar(50) not null,
+  length int not null,
+  maturity varchar(30) not null,
+  primary key (ContentID));
+
+-- Listener 
+create table listener (
+  AccountID int not null,
+  username Varchar(50) not null,
+  subscriptionID int not null,
+  primary key (AccountID),
+  Unique (username)
+  foreign key (AccountID)
+      references user(AccountID)
+      on update cascade
+      on delete cascade
+  foreign key (subscriptionID)
+      references subscription(SubscriptionID)
+      on update cascade
+      on delete cascade);
+
+-- creator
+  
+    
+  
+
